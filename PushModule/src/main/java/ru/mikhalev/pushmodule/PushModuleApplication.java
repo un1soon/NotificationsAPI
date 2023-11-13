92e9439bfdb9ff199aa5c9a8501c4d09ae76d25d
un1soon <pinmii@yandex.ru>
2023-11-13 13:12:41 +0400

    add-dockerfiles-and-docker-compose

--- PushModule/src/main/java/ru/mikhalev/pushmodule/PushModuleApplication.java
+++ PushModule/src/main/java/ru/mikhalev/pushmodule/PushModuleApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PushModuleApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PushModuleApplication.class, args);
     }
-
 }
